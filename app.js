94412e2332204d896bbd1d805ca4c81fbf6bea88
Diego DaSilva <diemasi129@gmail.com>
2023-04-25 21:05:13 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,61 +1,33 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
-const path = require('path');
+// Add event listener to form submit button to send the call transcription text to the server and display the summary
+document.getElementById('summarize-form').addEventListener('submit', async (e) => {
+  e.preventDefault();
 
-const app = express();
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
+  const transcription = document.getElementById('transcription').value;
+  const summaryElement = document.getElementById('summary');
+  
+  // Check if call transcription text exists
+  if (transcription) {
+    try {
+      // Send HTTP POST request to server API endpoint to generate the summary
+      const response = await fetch('/summarize', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ transcription }),
+      });
 
-// Replace with your OpenAI API key
-const OPENAI_API_KEY = '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';
-// Replace with the OpenAI API endpoint URL for the desired model
-const OPENAI_API_URL = 'https://api.openai.com/v1/engines/davinci-codex/completions';
-
-// Async function to summarize the call transcription text using OpenAI
-async function summarizeText(text) {
-  const data = {
-    prompt: `Please provide a summary of the following call transcription: ${text}`,
-    max_tokens: 100, // Adjust this value based on your desired summary length
-    n: 1,
-    stop: null,
-    temperature: 0.7,
-  };
-
-  const headers = {
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${OPENAI_API_KEY}`,
-  };
-
-  try {
-    const response = await axios.post(OPENAI_API_URL, data, { headers });
-    const summary = response.data.choices[0].text.trim();
-    return summary;
-  } catch (error) {
-    console.error('Error in generating summary:', error);
-    return null;
-  }
-}
-
-// API endpoint to generate the call transcription summary
-app.post('/summarize', async (req, res) => {
-  const callTranscription = req.body.transcription;
-
-  if (callTranscription) {
-    const summary = await summarizeText(callTranscription);
-
-    if (summary) {
-      res.json({ summary });
-    } else {
-      res.status(500).json({ error: 'Unable to generate summary' });
+      // If server returns successful response, display the summary
+      if (response.ok) {
+        const data = await response.json();
+        summaryElement.innerText = data.summary;
+      } else {
+        summaryElement.innerText = 'Error: Unable to generate summary';
+      }
+    } catch (error) {
+      summaryElement.innerText = 'Error: Unable to connect to the server';
     }
   } else {
-    res.status(400).json({ error: 'No transcription data found' });
+    summaryElement.innerText = 'Error: Please enter a call transcription';
   }
-});
-
-// Set up the server to listen on a specified port
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
 });
